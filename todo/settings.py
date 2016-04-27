a866d8b206b14ff6c11b55150a3ddb46c84c9586
Diego Humpire <dh@mambo.pe>
2016-04-27 11:18:58 -0500

    add whitenoise

--- todo/settings.py
+++ todo/settings.py
@@ -133,6 +133,8 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
